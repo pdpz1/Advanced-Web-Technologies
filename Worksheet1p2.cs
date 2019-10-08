dd3f0e2306fa2f9af00205f51f2d992abf10a974
pdpz1 <56076310+pdpz1@users.noreply.github.com>
2019-10-08 16:37:04 +0100

    Add files via upload

---
+++ Worksheet1p2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Sum1to10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i=1; i<=10; i++)
+            {
+                sum += i;
+            }
+            Console.Write(sum);
+        }
+    }
+}
