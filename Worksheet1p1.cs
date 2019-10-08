a50ba5f0516b9b2bf2358cb17610767f7fb0ab8b
pdpz1 <56076310+pdpz1@users.noreply.github.com>
2019-10-08 16:41:01 +0100

    Add files via upload

---
+++ Worksheet1p1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Worksheet1p1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name;
+            Console.WriteLine("Your name is: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
+        }
+    }
+}
