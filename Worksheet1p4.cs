28d2d6f06b6299ff70184a62616fe440800106f9
pdpz1 <56076310+pdpz1@users.noreply.github.com>
2019-10-08 17:30:29 +0100

    Add files via upload

---
+++ Worksheet1p4.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Worksheet1p3
+{
+    class Worksheet1p3
+    {
+        static void Main(string[] args)
+        {
+            int a, b, total = 0;
+            string x;
+            Console.WriteLine("Welcome to the adding calculator!");
+            Console.WriteLine("Please enter your first number: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter your second number: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter an operator: ");
+            x = Console.ReadLine();
+            if (x.Equals("+"))
+            {
+                total = a + b;
+            }
+            else if (x.Equals("-"))
+            {
+                total = a - b;
+            }
+            else if (x.Equals("*"))
+            {
+                total = a * b;
+            }
+            else if (x.Equals("/"))
+            {
+                total = a / b;
+            }
+            Console.WriteLine("The total is: " + total);
+
+        }
+    }
+}
+
